da77f5ceb98266cb2c1dcf37bb40f9084419d650
eric-r-albrecht <enrique3399@gmail.com>
2019-08-07 19:04:45 -0500

    Updates to push button demonstration.

--- main.c
+++ main.c
@@ -6,9 +6,30 @@
  */
 
 #include "gpio.h"
+#include "config.h"
+
+extern const io LED_1_0_IO;
+extern const io LED_1_6_IO;
+extern const io PUSH_BUTTON_IO;
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	io new_io;
-	return 0;
+
+	LED_1_0_IO.function_map->init(LED_1_0_IO.config);
+	LED_1_6_IO.function_map->init(LED_1_6_IO.config);
+	PUSH_BUTTON_IO.function_map->init(PUSH_BUTTON_IO.config);
+
+	LED_1_0_IO.function_map->set(LED_1_0_IO.config);
+	LED_1_6_IO.function_map->clear(LED_1_6_IO.config);
+
+	while(1==1){
+	    if(PUSH_BUTTON_IO.function_map->read(PUSH_BUTTON_IO.config) == ePin_On){
+	        LED_1_0_IO.function_map->set(LED_1_0_IO.config);
+	        LED_1_6_IO.function_map->clear(LED_1_6_IO.config);
+	    } else {
+	        LED_1_0_IO.function_map->clear(LED_1_0_IO.config);
+	        LED_1_6_IO.function_map->set(LED_1_6_IO.config);
+	    }
+	}
 }
